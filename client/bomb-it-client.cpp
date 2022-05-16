2ce2c83b7ea68b58ae15fdeab406945634332254
Piotr Ulanowski <pioula111@gmail.com>
2022-05-16 14:50:28 +0200

    parser with optional

--- client/bomb-it-client.cpp
+++ client/bomb-it-client.cpp
@@ -18,16 +18,16 @@ namespace po = boost::program_options;
 
 namespace parser {
     const string ERROR_MESSAGE = "Missing flag! Type ./bomb-it-client -h for help\n";
-    using values_t = variant<nullptr_t, po::typed_value<string>*, po::typed_value<uint16_t>*>;
+    using values_t = variant<po::typed_value<string>*, po::typed_value<uint16_t>*>;
 
     struct flag {
         string long_name;
         string short_name;
-        values_t value_type;
+        optional<values_t> value_type;
         string description;
     } flags[] = {
             { "gui-address", "d", po::value<string>(), "<(nazwa hosta):(port) lub (IPv4):(port) lub (IPv6):(port)>" },
-            { "help", "h", nullptr, "produce help message" },
+            { "help", "h", nullopt, "produce help message" },
             { "player-name", "n", po::value<string>(), "string" },
             { "port", "p", po::value<uint16_t>(), "Port na którym klient nasłuchuje komunikatów od GUI" },
             { "server-address", "s", po::value<string>(), "<(nazwa hosta):(port) lub (IPv4):(port) lub (IPv6):(port)>" }
@@ -39,12 +39,13 @@ namespace parser {
         for (const auto &flag: flags) {
             string flag_names = flag.long_name;
             flag_names.append(",").append(flag.short_name);
-            if (flag.value_type.index()) {
+
+            if (auto value_type = flag.value_type) {
                 string w;
                 visit([&](auto value){
                     desc.add_options()
                             (flag_names.c_str(), value, flag.description.c_str());
-                }, flag.value_type);
+                }, *value_type);
             }
             else {
                 desc.add_options()
@@ -88,51 +89,6 @@ namespace parser {
 
 int main(int argc, char *argv[]) {
     puts("Hello!");
-//    po::options_description desc("Allowed options");
-//    desc.add_options()
-//        ("gui-address,d", po::value<string>(), "<(nazwa hosta):(port) lub (IPv4):(port) lub (IPv6):(port)>");
-//    desc.add_options()
-//        ("help,h", "produce help message")
-//        ("player-name,n", po::value<string>(), "string")
-//        ("port,p", po::value<uint16_t>(), "Port na którym klient nasłuchuje komunikatów od GUI")
-//        ("server-address,s", po::value<string>(), "<(nazwa hosta):(port) lub (IPv4):(port) lub (IPv6):(port)>")
-//    ;
-//
-//    po::variables_map vm;
-//    po::store(po::parse_command_line(argc, argv, desc), vm);
-//    po::notify(vm);
-//    const string ERROR_MESSAGE = "Missing flag! Type ./bomb-it-client -h for help\n";
-//
-//
-//    if (vm.count("help")) {
-//        cout << desc << "\n";
-//        return 0;
-//    }
-//
-//    if (vm.count("gui-address")) {
-//        cout << "gui" << "\n";
-//    }
-//    else {
-//        cout << ERROR_MESSAGE;
-//        return 1;
-//    }
-//
-//    if (vm.count("player-name")) {
-//        cout << "player-name" << "\n";
-//    }
-//    else {
-//        cout << ERROR_MESSAGE;
-//        return 1;
-//    }
-//
-//    if (vm.count("port")) {
-//        cout << "port" << "\n";
-//    }
-//    else {
-//        cout << ERROR_MESSAGE;
-//        return 1;
-//    }
-
     parser::parse_command_line(argc, argv);
     cout << "End!" << endl;
 }
